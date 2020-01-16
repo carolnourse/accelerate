1fe98c549e424f8e5d9d5680506f17cba94133dc
Carol Nourse <carolnourse@hotmail.com>
2020-01-16 18:55:13 +0000

    custom post types, custom fields, home page, custom archives, social media menu, importing blogs

--- wp-content/themes/accelerate-theme/header.php
+++ wp-content/themes/accelerate-theme/header.php
@@ -16,7 +16,7 @@
 	<meta name="description" content="A WordPress portfolio project site, built with Skillcrush, featuring custom post types, custom queries, and custom fields.">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700|Open+Sans:400,600,700&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
